2d9706fb19f76f7144c7f19f67d3cd718cfb8826
miyazato1872 <miyazato5997@gmail.com>
2023-01-30 13:44:07 +0900

    コントローラーとindexのビューファイル作成
    ルートパスで
    app/views/items/index.html.erb
    に遷移するように設定済み

--- db/migrate/20230130040620_create_items.rb
+++ db/migrate/20230130040620_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string     :name,              null: false
       t.integer    :place_category_id, null: false
       t.text       :memo,              null: false
-      t.references :user,              null: false, foreign_key: true
+      # t.references :user,              null: false, foreign_key: true
       t.timestamps
     end
   end
