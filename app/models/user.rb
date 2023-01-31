008cf128018d518b8af4ceed408e30d465f40603
miyazato1872 <miyazato5997@gmail.com>
2023-01-31 11:12:51 +0900

    ユーザー管理機能の編集
    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true  # 空では登録できない
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, allow_blank: true  # 半角英数字混合での入力が必須
 end
